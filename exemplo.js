e4ba6c99c5e33ba297a2c30711795ebf78d4331b
HENRIQUE SOARES PEREIRA <48878495832@info.local>
2024-05-09 08:55:34 -0300

    Feat 2: Exercicio 2 feito

--- exemplo.js
+++ exemplo.js
@@ -14,13 +14,13 @@
 
 //fazer uma conta com o Java script
 
-let nota1 = parseInt(prompt("Digite uma nota: "))
+// let nota1 = parseInt(prompt("Digite uma nota: "))
 
-let nota2 = parseInt(prompt("Digite outr nota: "))
+// let nota2 = parseInt(prompt("Digite outr nota: "))
 
-let media = (nota1 + nota2) /2
+// let media = (nota1 + nota2) /2
 
-console.log(media)
+// console.log(media)
 
 //parselfloat => transforma oque esta entre os parenteses em numero co virgula
 
@@ -50,4 +50,40 @@ console.log(media)
 // else{
 //     console.log ("Voce não tem permissão de acesso!")
 // }
+
+
+// Exercício 2: Verificação de dia da semana
+// Escreva um programa que solicita ao usuário um número de 1 a 7 representando um dia da
+// semana e exibe o nome correspondente a esse dia. Considere que 1 representa segunda-feira,
+// 2 representa terça-feira e assim por diante.
+
+let numero = prompt("Digite um numero de 1 a 7")
+
+if(numero == "1"){
+    console.log("Segunda-feira")    
+}
+
+if(numero == "2"){
+    console.log("Terça-feira")    
+}
+
+if(numero == "3"){
+    console.log("Quarta-feira")    
+}
+
+if(numero == "4"){
+    console.log("Quinta-feira")    
+}
+
+if(numero == "5"){
+    console.log("Sexta-feira")    
+}
+
+if(numero == "6"){
+    console.log("Sabado")    
+}
+
+if(numero == "7"){
+    console.log("Domingo")    
+}
 
