67d22bc92b54b858dbb977fa4f1a321bcbedc099
HENRIQUE SOARES PEREIRA <48878495832@info.local>
2024-05-09 09:21:43 -0300

    Feat 3:exercicio 2 concluido

---
+++ exc2.js
@@ -0,0 +1,34 @@
+// Exercício 2: Verificação de dia da semana
+// Escreva um programa que solicita ao usuário um número de 1 a 7 representando um dia da
+// semana e exibe o nome correspondente a esse dia. Considere que 1 representa segunda-feira,
+// 2 representa terça-feira e assim por diante.
+
+let numero = prompt("Digite um numero de 1 a 7")
+
+if(numero == "1"){
+    console.log("Segunda-feira")    
+}
+
+if(numero == "2"){
+    console.log("Terça-feira")    
+}
+
+if(numero == "3"){
+    console.log("Quarta-feira")    
+}
+
+if(numero == "4"){
+    console.log("Quinta-feira")    
+}
+
+if(numero == "5"){
+    console.log("Sexta-feira")    
+}
+
+if(numero == "6"){
+    console.log("Sabado")    
+}
+
+if(numero == "7"){
+    console.log("Domingo")    
+}
