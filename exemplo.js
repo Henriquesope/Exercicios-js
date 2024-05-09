8ca534ddac67dabbbbb4f0587b1cf5115033ccd3
HENRIQUE SOARES PEREIRA <48878495832@info.local>
2024-05-09 08:43:20 -0300

    Feat: exercicio 1 feito

---
+++ exemplo.js
@@ -0,0 +1,53 @@
+//apresentar alguma mensagem na tela
+// alert("Helloword")
+
+//apresenta mensagem no console
+// console.log("console: Hello word")
+
+//apresent auma mensagem e pega informacao
+// prompt("")
+
+//Pegando um nome e apresentando na tela
+
+// let nome = prompt("Digite o seu nome:");
+// console.log("seja bem vindo" + nome)
+
+//fazer uma conta com o Java script
+
+let nota1 = parseInt(prompt("Digite uma nota: "))
+
+let nota2 = parseInt(prompt("Digite outr nota: "))
+
+let media = (nota1 + nota2) /2
+
+console.log(media)
+
+//parselfloat => transforma oque esta entre os parenteses em numero co virgula
+
+
+
+
+
+
+
+
+
+
+
+
+// Exercício 1: Verificação de login
+// Escreva um programa que solicita ao usuário um nome de usuário e uma senha. Verifique se o
+// nome de usuário é &quot;admin&quot; e a senha é &quot;senha123&quot;. Exiba uma mensagem indicando se o login
+// foi bem-sucedido ou não.
+
+// let username = prompt("Digete o seu usuário");
+
+// let senha = prompt("Digite sua senha");
+
+// if(username == "admin" && senha == "senha123"){
+//     console.log("Login be sucedido!")
+// }
+// else{
+//     console.log ("Voce não tem permissão de acesso!")
+// }
+
